3f18335e1cc968664f6ddc1fab38098781e8cfa6
sandyrod <sandyrod@gmail.com>
2025-03-24 15:26:53 -0400

    Agregar main a vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/style.css','resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/style.css','resources/css/app.css', 'resources/js/app.js', 'resources/js/main.js'],
             refresh: true,
         }),
         tailwindcss(),
