1f92fb0bdc72820bb0f08b63c9e8bfd06a08ad45
sandyrod <sandyrod@gmail.com>
2025-03-31 18:24:39 -0400

    Faq model

--- app/Models/faq.php
+++ app/Models/faq.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class faq extends Model
+class Faq extends Model
 {
     use HasFactory;
 
